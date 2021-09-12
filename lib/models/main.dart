ed825de03843c32e945594e4194a76d6e2f8d07f
ttpaik <ttpaik@bu.edu>
2021-09-12 18:56:44 -0400

    new commit

---
+++ lib/models/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+const API_PREFIX = 'k7OIzZdhsCq7Ugqfl8kHX6XDrBjFFhvTY0PfDXkz';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Barcode Scanner',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'Barcode Scanner'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String fdcID = 'Unknown';
+  String nutInfo = 'Unknown';
+  String calories = 'Unknown';
+  String barcodeScan = 'Unknown';
+
+  // void _incrementCounter() {
+  //   setState(() {
+  //     // This call to setState tells the Flutter framework that something has
+  //     // changed in this State, which causes it to rerun the build method below
+  //     // so that the display can reflect the updated values. If we changed
+  //     // _counter without calling setState(), then the build method would not be
+  //     // called again, and so nothing would appear to happen.
+  //     _counter++;
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'barcode number: $barcodeScan'),
+            Text(
+              'Nutrition Info: $nutInfo',),
+            Text(
+              'FDC ID: $fdcID',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              'Calories: $calories Kcal.',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton.extended(
+            icon: Icon(Icons.add),
+            onPressed: scanBarcode,
+            tooltip: 'barcode',
+            label: Text('Scan Barcode')
+          ),
+          FloatingActionButton.extended(
+            icon: Icon(Icons.add),
+            onPressed: getNutrition,
+            tooltip: 'nut_info',
+            label: Text('Get Nutrition Info'),
+          )
+      ]
+      ),
+    );
+  }
+  Future<void> scanBarcode() async {
+    try {
+      final barcodeScan = await FlutterBarcodeScanner.scanBarcode(
+        "#ff6666",
+        "Cancel",
+        true,
+        ScanMode.BARCODE,
+      );
+
+      if (!mounted) return;
+
+      setState(() {
+        this.barcodeScan = barcodeScan;
+      });
+    } on PlatformException {
+      barcodeScan = 'Failed to get platform version.';
+    }
+  }
+
+  Future<void> getNutrition() async {
+      var response = await http.get(Uri.parse('https://api.nal.usda.gov/fdc/v1/foods/search?query=$barcodeScan&api_key=' + API_PREFIX));
+      var nut = jsonDecode(response.body);
+      fdcID = nut['foods'][0]['fdcId'].toString();
+      nutInfo = nut['foods'][0]['description'].toString();
+      calories = nut['foods'][0]['foodNutrients'][3]['value'].toString();
+
+      setState(() {
+            this.nutInfo = nutInfo;
+            this.fdcID = fdcID;
+            this.calories = calories;
+          });
+    }
+  }
