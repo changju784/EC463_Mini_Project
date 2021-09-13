c2a0437b623ddc23d406e83e12f914b39de412ac
jungin <changju@bu.edu>
2021-09-13 14:10:50 +0900

    UI revised

--- lib/models/main.dart
+++ lib/models/main.dart
@@ -1,17 +1,49 @@
 import 'dart:ffi';
-
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:http/http.dart' as http;
-import 'dart:async';
+
 import 'dart:convert';
+
 const API_PREFIX = 'k7OIzZdhsCq7Ugqfl8kHX6XDrBjFFhvTY0PfDXkz';
 
-void main() {
-  runApp(MyApp());
-}
 
+void main() {
+  runApp(new MaterialApp(
+    home: new SplashScreen(),
+    routes: <String, WidgetBuilder>{
+      '/HomeScreen': (BuildContext context) => new MyApp()
+    },
+  ));
+}
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 4);
+    return new Timer(_duration, navigationPage);
+  }
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/HomeScreen');
+  }
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Image.asset('assets/splash.png'),
+      ),
+    );
+  }
+}
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -19,15 +51,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Barcode Scanner',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
       ),
       home: MyHomePage(title: 'Barcode Scanner'),
@@ -37,8 +60,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
   final String title;
 
   @override
@@ -66,18 +87,14 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'barcode number: $barcodeScan'),
+                'barcode number: $barcodeScan'),
             Text(
               'Nutrition Info: $nutInfo',),
             Text(
@@ -92,21 +109,21 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton.extended(
-            icon: Icon(Icons.add),
-            onPressed: scanBarcode,
-            tooltip: 'barcode',
-            label: Text('Scan Barcode')
-          ),
-          FloatingActionButton.extended(
-            icon: Icon(Icons.add),
-            onPressed: getNutrition,
-            tooltip: 'nut_info',
-            label: Text('Get Nutrition Info'),
-          )
-      ]
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton.extended(
+                icon: Icon(Icons.add),
+                onPressed: scanBarcode,
+                tooltip: 'barcode',
+                label: Text('Scan Barcode')
+            ),
+            FloatingActionButton.extended(
+              icon: Icon(Icons.add),
+              onPressed: getNutrition,
+              tooltip: 'nut_info',
+              label: Text('Get Nutrition Info'),
+            )
+          ]
       ),
     );
   }
@@ -130,16 +147,16 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> getNutrition() async {
-      var response = await http.get(Uri.parse('https://api.nal.usda.gov/fdc/v1/foods/search?query=$barcodeScan&api_key=' + API_PREFIX));
-      var nut = jsonDecode(response.body);
-      fdcID = nut['foods'][0]['fdcId'].toString();
-      nutInfo = nut['foods'][0]['description'].toString();
-      calories = nut['foods'][0]['foodNutrients'][3]['value'].toString();
+    var response = await http.get(Uri.parse('https://api.nal.usda.gov/fdc/v1/foods/search?query=$barcodeScan&api_key=' + API_PREFIX));
+    var nut = jsonDecode(response.body);
+    fdcID = nut['foods'][0]['fdcId'].toString();
+    nutInfo = nut['foods'][0]['description'].toString();
+    calories = nut['foods'][0]['foodNutrients'][3]['value'].toString();
 
-      setState(() {
-            this.nutInfo = nutInfo;
-            this.fdcID = fdcID;
-            this.calories = calories;
-          });
-    }
+    setState(() {
+      this.nutInfo = nutInfo;
+      this.fdcID = fdcID;
+      this.calories = calories;
+    });
   }
+}
